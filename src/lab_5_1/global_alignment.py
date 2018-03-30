a1fc38ae808dd02d99c957595dc848d01fa57898
Oleg Ovcharuk <vgvoleg@mail.ru>
2018-03-30 23:15:50 +0300

    lab 5_1 wtf it works??

--- src/lab_5_1/global_alignment.py
+++ src/lab_5_1/global_alignment.py
@@ -6,45 +6,45 @@ FORFEIT = 5
 def global_alignment(v, w, score_matrix):
     n = len(v) + 1
     m = len(w) + 1
-
     s = [[0] * m for i in range(0, n)]
-    b = [['?'] * m for i in range(0, n)]
 
     for i in range(1, n):
         for j in range(1, m):
             vi = v[i - 1]
             wi = w[j - 1]
-
             s[i][j] = max( \
                 s[i - 1][j] - FORFEIT, \
                 s[i][j - 1] - FORFEIT, \
                 s[i - 1][j - 1] + utils.get_score(vi, wi, score_matrix) \
             )
-            if s[i][j] == s[i - 1][j - 1] + utils.get_score(vi, wi, score_matrix):
-                b[i][j] = "↖"
-            elif s[i][j] == s[i - 1][j] - FORFEIT:
-                b[i][j] = "↑"
-            else:
-                b[i][j] = "←"
 
     words = {"v": '', "w": ''}
     restore_words(words, s, v, w, n - 1, m - 1)
     words["v"] = words["v"][::-1]
     words["w"] = words["w"][::-1]
-    print(words)
+    print(words["v"])
+    print(words["w"])
 
-
+ # TODO: refactor
 def restore_words(words, s, v, w, i, j):
-    if s[i - 1][j - 1] >= s[i - 1][j] and s[i - 1][j - 1] >= s[i][j - 1]:
-        words["v"] += v[i - 1]
-        words["w"] += w[j - 1]
+    if i == 0 and j == 0:
+        return
+    if s[i - 1][j - 1] >= s[i - 1][j] - FORFEIT and s[i - 1][j - 1] >= s[i][j - 1] - FORFEIT:
+        next_i = i - 1
+        next_j = j - 1
         if i == 0:
             words["v"] += '-'
-            return
-        elif j == 0:
+            next_i = i
+        else:
+            words["v"] += v[i - 1]
+
+        if j == 0:
             words["w"] += '-'
-            return
-        restore_words(words, s, v, w, i - 1, j - 1)
+            next_j = j
+        else:
+            words["w"] += w[j - 1]
+
+        restore_words(words, s, v, w, next_i, next_j)
     else:
         if i == 0 or j == 0:
             return
