60e7135ae4c97ead36e26c99217ee089354188e5
Oleg Ovcharuk <vgvoleg@mail.ru>
2018-04-05 00:38:52 +0300

    euler path

--- src/lab_7_2/euler_path.py
+++ src/lab_7_2/euler_path.py
@@ -20,21 +20,46 @@ def find_cycle(graph, start):
     cycle.append(current)
     return cycle
 
-def euler_cycle(strings):
+def add_extra_edge_to_graph(graph):
+    from_to = {}
+    for node_from in graph:
+        if node_from not in from_to:
+            from_to[node_from] = {"from": 0, "to": 0}
+        for node_to in graph[node_from]:
+            if node_to not in from_to:
+                from_to[node_to] = {"from": 0, "to": 0}
+            from_to[node_from]["from"] += 1
+            from_to[node_to]["to"] += 1
+    odd_from = ''
+    odd_to = ''
+    for node in from_to:
+        if from_to[node]["from"] < from_to[node]["to"]:
+            odd_from = node
+        elif from_to[node]["from"] > from_to[node]["to"]:
+            odd_to = node
+    if odd_from not in graph:
+        graph[odd_from] = []
+    graph[odd_from].append(odd_to)
+    return odd_from, odd_to
+
+def euler_path(strings):
     graph = {}
     for string in strings:
         frm = string.split(' -> ')[0]
         to = string.split(' -> ')[1].split(',')
         graph[frm] = to
+    frm, to = add_extra_edge_to_graph(graph)
     cycle = []
     while not empty(graph):
-        start = max(graph, key=lambda k: len(graph[k])) if len(cycle) == 0 \
+        start = frm if len(cycle) == 0 \
             else max(cycle, key=lambda k: len(graph[k]))
         new_cycle = find_cycle(graph, start)
         cycle = new_cycle if len(cycle) == 0 else append_cycle(cycle, new_cycle)
+    cycle.pop(0)
     return cycle
+
 
 if __name__ == '__main__':
     strings = utils.read_input_data()
-    result = euler_cycle(strings)
+    result = euler_path(strings)
     utils.write_output_data('->'.join(result))
